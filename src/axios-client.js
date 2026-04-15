daad199cca1b06cffa3f449264abd032487396ce
ZainulAbidin-dot <zchxvr@gmail.com>
2026-04-15 21:49:38 +0300

    ...

--- src/axios-client.js
+++ src/axios-client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BACKEND_URL = 'https://family-tree-backend-production-630e.up.railway.app';
+export const BACKEND_URL = 'https://family-tree-backend-t1qq.onrender.com';
 // export const BACKEND_URL = 'http://localhost:3005';
 
 export const axiosClient = axios.create({
